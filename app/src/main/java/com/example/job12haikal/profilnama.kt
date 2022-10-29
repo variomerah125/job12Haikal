f48c4ba8f5980ab2b3ce9fce0285d79e1ca71cf0
jobsheet9 <rgagas100@gmail.com>
2022-10-29 19:57:15 +0700

    first commit

---
+++ app/src/main/java/com/example/job12haikal/profilnama.kt
@@ -0,0 +1,11 @@
+package com.example.job12haikal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profilnama : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profilnama)
+    }
+}
